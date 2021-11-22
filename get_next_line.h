4bd03a9938467da618c288cbd71a6dcbf485c905
Your Name <you@ex>
2021-11-22 11:52:31 +0000

    100%

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fstupar <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 09:53:13 by fstupar           #+#    #+#             */
+/*   Updated: 2021/11/22 10:20:30 by fstupar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*read_to_string(int fd, char *string);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+size_t	ft_strlen(const char *s);
+char	*get_line(char *string);
+char	*last_step(char *string);
+
+#endif
